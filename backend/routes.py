f5984fc45aef0bdedc39bf4c1f64e52f29ba1c4c
Vincent Bourgeois <vincent_bourgeois@natural-solutions.eu>
2018-10-10 17:29:46 +0200

    Dynamic comparator

--- backend/routes.py
+++ backend/routes.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from flask import Flask, render_template
 import models
+import random
 from models import (db)
 
 dicotheme_schema = models.DicoThemeSchema(many=True)
@@ -17,6 +18,23 @@ def home():
 
 @main.route('/comparateur')
 def comparateur():
+
+    def getPhoto(n):
+        mdSizes = ['450/300', '300/450']
+        lgSizes = ['900/600', '600/900']
+        randSize = n % 2
+        return {
+            'sm': 'https://picsum.photos/200?image=%s' % n,
+            'md': 'https://picsum.photos/%s?image=%s' % (mdSizes[randSize], n),
+            'lg': 'https://picsum.photos/%s?image=%s' % (lgSizes[randSize], n),
+            'date': '%s/%s/%s' % (random.randrange(1900, 2018), random.randrange(1, 12), random.randrange(10, 28))
+        }
+
+
     all_dicos = models.DicoTheme.query.all()
     result = dicotheme_schema.dump(all_dicos)
-    return render_template('comparateur.html', titre="Bienvenue !", mots=result)
+    photos = [getPhoto(i) for i in range(10, 16)]
+    site = {
+        'photos': photos
+    }
+    return render_template('comparateur.html', titre="Bienvenue !", mots=result, site=site)
--- backend/static/js/comparator.js
+++ backend/static/js/comparator.js
@@ -1,12 +1,31 @@
-console.log('ready addEventListener');
-
-document.onreadystatechange = () => {
-  if (document.readyState == 'complete') {
-    new Vue({
-      el: '#js-app-comparator',
-      data: {
-        title: "title toto"
+initComparator = (options) => {
+  let nextSelectedIndex = 0;
+  new Vue({
+    el: '#js-app-comparator',
+    data: () => {
+      return {
+        selectedPhotos: [options.site.photos[0], options.site.photos[1]],
+        zoomPhotos: []
       }
-    });
-  }
-};
+    },
+    methods: {
+      onThumbClick(i) {
+        this.$set(this.selectedPhotos, nextSelectedIndex, Object.assign({
+          comparedLoaded: false
+        }, options.site.photos[i]))
+        nextSelectedIndex = ++nextSelectedIndex % 2
+      },
+      onComparedLoaded(i) {
+        this.$set(this.selectedPhotos, i, Object.assign(this.selectedPhotos[i], {
+          comparedLoaded: true
+        }))
+      },
+      onZoomClick(i) {
+        if (i === undefined)
+          this.zoomPhotos = this.selectedPhotos
+        else
+          this.zoomPhotos = [this.selectedPhotos[i]]
+      }
+    }
+  })
+}
