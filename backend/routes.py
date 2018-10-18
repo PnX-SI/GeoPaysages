f3a852e6163c089549e9540644cac3e252493143
HamoudaAmine <amine.hamouda@gmail.com>
2018-10-18 17:34:47 +0200

    add thumbnail & waterMark

--- backend/routes.py
+++ backend/routes.py
@@ -1,14 +1,40 @@
 # coding: utf-8
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, Blueprint
 import models
 import random
 from models import (db)
+from config import DATA_IMAGES_PATH
+from PIL import Image, ImageFont, ImageDraw
+import os
 
 dicotheme_schema = models.DicoThemeSchema(many=True)
-
-from flask import Blueprint
 main = Blueprint('main', __name__, template_folder='tpl')
 
+def getThumbnail(imageUrl):
+    base_img_url = DATA_IMAGES_PATH
+    imageThumbnailUrl = base_img_url+'thumbnail_'+imageUrl
+    imageUrl = base_img_url+imageUrl
+    if not (os.path.exists(imageThumbnailUrl)):
+        try:
+            image = Image.open(imageUrl)
+            image.thumbnail((200, 200))
+            image.save(imageThumbnailUrl)
+        except Exception:
+            print('getThumbnail Invalid image')
+
+def addWatherMark(imageUrl):
+    font = ImageFont.truetype("./static/fonts/openSans.ttf",14)
+    base_img_url = DATA_IMAGES_PATH
+    imageCopyright = base_img_url+'copyrigh_'+imageUrl
+    if not (os.path.exists(imageCopyright)):
+        try:
+            image = Image.open(base_img_url+imageUrl)
+            draw = ImageDraw.Draw(image)
+            width, height = image.size
+            draw.text((10, height-24),"© Copyright 2018 by Parc National de la Vanoise",font=font,fill=(255,255,255,255))
+            image.save(imageCopyright)
+        except Exception:
+            print('addWatherMark Invalid image')
 
 @main.route('/')
 def home():
