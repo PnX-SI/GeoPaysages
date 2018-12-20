5eba39d7c124588b483391d72665a4abffb36f2f
Vincent Bourgeois <vincent_bourgeois@natural-solutions.eu>
2018-12-21 00:02:08 +0100

    Fix home missing main photo, use a dedicated image format

--- backend/routes.py
+++ backend/routes.py
@@ -45,15 +45,29 @@ def home():
         sites.append(sites[x])
 
     photo_ids = []
+    sites_without_photo = []
     code_communes = []
     for site in sites:
-        photo_ids.append(site.get('main_photo'))
+        photo_id = site.get('main_photo')
+        if photo_id:
+            photo_ids.append(site.get('main_photo'))
+        else:
+            sites_without_photo.append(str(site.get('id_site')))
         code_communes.append(site.get('code_city_site'))
 
     query_photos = models.TPhoto.query.filter(
         models.TPhoto.id_photo.in_(photo_ids)
     )
     dump_photos = photo_schema.dump(query_photos).data
+
+    if len(sites_without_photo):
+        sql_missing_photos_str = "select distinct on (id_site) * from geopaysages.t_photo where id_site IN (" + ",".join(sites_without_photo) + ") order by id_site, filter_date desc"
+        sql_missing_photos = text(sql_missing_photos_str)
+        missing_photos_result = db.engine.execute(sql_missing_photos).fetchall()
+        missing_photos = [dict(row) for row in missing_photos_result]
+        for missing_photo in missing_photos:
+            missing_photo['t_site'] = missing_photo.get('id_site')
+            dump_photos.append(missing_photo)
 
     query_commune = models.Communes.query.filter(
         models.Communes.code_commune.in_(code_communes)
@@ -63,9 +77,7 @@ def home():
     for site in sites:
         id_site = site.get('id_site')
         photo = next(photo for photo in dump_photos if (photo.get('t_site') == id_site))
-        site['photo'] = url_for(
-            'static', filename=DATA_IMAGES_PATH + photo.get('path_file_photo')
-        )
+        site['photo'] = utils.getThumbnail(photo, 400).get('output_url')
         site['commune'] = next(commune for commune in dump_communes if (commune.get('code_commune') == site.get('code_city_site')))
 
 
