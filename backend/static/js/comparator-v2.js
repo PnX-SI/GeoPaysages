2a244eb073a26833f98d776346df64551a439fa6
Vincent <vincent_bourgeois@natural-solutions.eu>
2020-08-11 16:11:28 +0200

    Test compare with leaflet-side-by-side.js

--- backend/static/js/comparator-v2.js
+++ backend/static/js/comparator-v2.js
@@ -1,12 +1,16 @@
 var geopsg = geopsg || {};
 geopsg.comparator = (options) => {
-  let photoMap1;
-  let photoMap2;
   const maps = [];
+  const MODES = {
+    SIDE_BY_SIDE: 'sbs',
+    SPLIT: 'split'
+  };
   new Vue({
     el: '#js-app-comparator',
     data: () => {
       return {
+        MODES: MODES,
+        curMode: MODES.SIDE_BY_SIDE,
         photos: options.photos,
         comparedPhotos: [
           options.photos[0],
@@ -25,6 +29,10 @@ geopsg.comparator = (options) => {
         maps[0].sync(maps[1]);
         maps[1].sync(maps[0]);
 
+        // Init L.control.sideBySide
+        maps[0].createPane('left');
+        maps[0].createPane('right');
+
         this.updateLayers();
       },
       initMap(num) {
@@ -40,6 +48,11 @@ geopsg.comparator = (options) => {
 
         return map;
       },
+      onPhotoSelected(index, photo) {
+        this.$set(this.comparedPhotos, index, photo);
+        //this.comparedPhotos[index] = photo;
+        this.updateLayers();
+      },
       updateLayers() {
         this.clearMaps();
 
@@ -48,12 +61,24 @@ geopsg.comparator = (options) => {
           this.loadImg(this.comparedPhotos[1].filename)
         ])
           .then(imgs => {
-            maps.forEach((map, i) => {
-              const img = imgs[i];
+            const layers = [];
+            imgs.forEach((img, i) => {
               const imgW = img.width;
               const imgH = img.height;
-              L.imageOverlay(img.src, [[-imgH / 2, -imgW / 2], [imgH / 2, imgW / 2]]).addTo(map);
+              layers.push(
+                L.imageOverlay(img.src, [[-imgH / 2, -imgW / 2], [imgH / 2, imgW / 2]])
+              );
             });
+            if (this.curMode == MODES.SPLIT) {
+              layers[0].addTo(maps[0]);
+              layers[1].addTo(maps[1]);
+            } else if (this.curMode == MODES.SIDE_BY_SIDE) {
+              layers[0].options.pane = 'left';
+              layers[1].options.pane = 'right';
+              layers[0].addTo(maps[0]);
+              layers[1].addTo(maps[0]);
+              L.control.sideBySide(layers[0], layers[1]).addTo(maps[0]);
+            }
           });
       },
       loadImg(filename) {
@@ -71,11 +96,6 @@ geopsg.comparator = (options) => {
             map.removeLayer(layer);
           });
         });
-      },
-      onPhotoSelected(index, photo) {
-        this.$set(this.comparedPhotos, index, photo);
-        //this.comparedPhotos[index] = photo;
-        this.updateLayers();
       }
     }
   })
