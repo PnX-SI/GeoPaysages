ed5d9d1b6cfdfcf033ec7df3acb1380e71c9b6f5
Vincent Bourgeois <vincent_bourgeois@natural-solutions.eu>
2022-06-15 09:40:16 +0200

    fix: map marker take observatory color

--- backend/static/js/sites.js
+++ backend/static/js/sites.js
@@ -37,6 +37,23 @@ geopsg.initSites = (options) => {
   });
   const observatories = filterObservatories ? filterObservatories.items : null;
 
+  const getMarkerIcon = (site) => {
+    return L.divIcon({
+      html: `
+    <svg style="color: ${site.observatory.color}" width="40" height="40" viewBox="00 0 511.974 511.974">
+      <path fill="currentColor" d="M255.987,0C161.882,0,85.321,76.553,85.321,170.641c0,45.901,14.003,73.924,33.391,112.708
+			c3.012,6.025,6.17,12.331,9.438,19.038c47.753,98.065,120.055,204.792,120.781,205.858c1.587,2.33,4.233,3.729,7.057,3.729
+			s5.47-1.399,7.057-3.729c0.725-1.067,73.028-107.793,120.781-205.858c3.268-6.707,6.426-13.013,9.438-19.038
+			c19.388-38.784,33.391-66.807,33.391-112.708C426.654,76.553,350.093,0,255.987,0z M332.19,180.233
+			"/>
+    </svg>`,
+      className: '',
+      iconSize: [40, 40],
+      iconAnchor: [18, 40],
+      popupAnchor: [0, -40],
+    });
+  };
+
   new Vue({
     el: '#js-app-sites',
     data: () => {
@@ -110,9 +127,7 @@ geopsg.initSites = (options) => {
           observatories.forEach((observatory) => {
             L.geoJson(wellknown.parse(observatory.data.geom), {
               style: {
-                color: observatory.data.color,
-                weight: 2,
-                opacity: 1,
+                opacity: 0,
                 fillColor: observatory.data.color,
                 fillOpacity: 0.3,
               },
@@ -263,7 +278,7 @@ geopsg.initSites = (options) => {
         selectedSites.forEach((site) => {
           lats.push(site.latlon[0]);
           lons.push(site.latlon[1]);
-          let marker = L.marker(site.latlon);
+          let marker = L.marker(site.latlon, { icon: getMarkerIcon(site) });
           site.marker = marker;
           markerText = site.name_site + '<br />' + site.ville.label;
           if (site.ref_site) {
