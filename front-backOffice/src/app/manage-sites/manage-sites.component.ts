6923a9fdf0cea3091fe1d4ec0fccd1a6969839d7
Jeje2201 <jeremy_leriche@natural-solutions.eu>
2022-08-03 14:58:20 +0200

    le ctrl s qui change l'indentation en premier

--- front-backOffice/src/app/manage-sites/manage-sites.component.ts
+++ front-backOffice/src/app/manage-sites/manage-sites.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, ChangeDetectorRef, NgZone, OnDestroy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectorRef,
+  NgZone,
+  OnDestroy,
+} from '@angular/core';
 import { tileLayer, latLng, Map, Layer, marker } from 'leaflet';
 import { SitesService } from '../services/sites.service';
 import * as _ from 'lodash';
@@ -10,156 +16,171 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
-	selector: 'app-manage-sites',
-	templateUrl: './manage-sites.component.html',
-	styleUrls: [ './manage-sites.component.scss' ]
+  selector: 'app-manage-sites',
+  templateUrl: './manage-sites.component.html',
+  styleUrls: ['./manage-sites.component.scss'],
 })
 export class ManageSitesComponent implements OnInit, OnDestroy {
-	map;
-	options = {
-		layers: [ tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png') ],
-		zoom: 10,
-		center: latLng(45.372167, 6.819077)
-	};
-	rows = [];
-	sitesLoaded = false;
-	markers: Layer[] = [];
-	center = latLng(45.372167, 6.819077);
-	zoom: number;
+  map;
+  options = {
+    layers: [tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')],
+    zoom: 10,
+    center: latLng(45.372167, 6.819077),
+  };
+  rows = [];
+  sitesLoaded = false;
+  markers: Layer[] = [];
+  center = latLng(45.372167, 6.819077);
+  zoom: number;
 
-	constructor(
-		private siteService: SitesService,
-		protected router: Router,
-		private changeDetector: ChangeDetectorRef,
-		private spinner: NgxSpinnerService,
-		private toastr: ToastrService,
-		private zone: NgZone
-	) {}
+  constructor(
+    private siteService: SitesService,
+    protected router: Router,
+    private changeDetector: ChangeDetectorRef,
+    private spinner: NgxSpinnerService,
+    private toastr: ToastrService,
+    private zone: NgZone
+  ) {}
 
-	ngOnInit() {
-		this.getAllSites();
-	}
+  ngOnInit() {
+    this.getAllSites();
+  }
 
-	onMapReady(map: Map) {
-		L.control.scale().addTo(map);
-		const street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-		const ignLayer = L.tileLayer(this.layerUrl(Conf.ign_Key, 'GEOGRAPHICALGRIDSYSTEMS.MAPS'));
-		const baseLayers = {
-			'IGN ': ignLayer,
-			OSM: street
-		};
-		L.control.layers(baseLayers).addTo(map);
+  onMapReady(map: Map) {
+    L.control.scale().addTo(map);
+    const street = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    );
+    const ignLayer = L.tileLayer(
+      this.layerUrl(Conf.ign_Key, 'GEOGRAPHICALGRIDSYSTEMS.MAPS')
+    );
+    const baseLayers = {
+      'IGN ': ignLayer,
+      OSM: street,
+    };
+    L.control.layers(baseLayers).addTo(map);
 
-		this.map = map;
-		const info = new L.Control();
-		info.setPosition('topleft');
-		info.onAdd = () => {
-			const container = L.DomUtil.create('button', ' btn btn-sm btn-outline-shadow leaflet-bar leaflet-control ');
-			container.innerHTML = '<i style="line-height: unset" class="icon-full_screen"> </i>';
-			container.style.backgroundColor = 'white';
-			container.title = 'Recenter la catre';
-			container.onclick = () => {
-				this.center = latLng(45.372167, 6.819077);
-				this.zoom = 10;
-			};
-			return container;
-		};
-		info.addTo(map);
-	}
+    this.map = map;
+    const info = new L.Control();
+    info.setPosition('topleft');
+    info.onAdd = () => {
+      const container = L.DomUtil.create(
+        'button',
+        ' btn btn-sm btn-outline-shadow leaflet-bar leaflet-control '
+      );
+      container.innerHTML =
+        '<i style="line-height: unset" class="icon-full_screen"> </i>';
+      container.style.backgroundColor = 'white';
+      container.title = 'Recenter la catre';
+      container.onclick = () => {
+        this.center = latLng(45.372167, 6.819077);
+        this.zoom = 10;
+      };
+      return container;
+    };
+    info.addTo(map);
+  }
 
-	getAllSites() {
-		this.spinner.show();
-		this.siteService.getAllSites().subscribe(
-			(sites) => {
-				_.forEach(sites, (site) => {
-					const newMarker = marker(site.geom, {
-						icon: L.icon({
-							iconSize: [ 25, 41 ],
-							iconAnchor: [ 13, 41 ],
-							iconUrl: './assets/marker-icon.png',
-							shadowUrl: './assets/marker-shadow.png'
-						})
-					});
-					const customPopup =
-						'<div class="title">' +
-						site.name_site +
-						'</div>' +
-						'<div class=""> <img src=' +
-						Conf.img_srv + 'crop?file=' +  site.main_photo + '&width=80&height=80&type=jpeg' +
-						'> </div>';
-					const customOptions = {
-						className: 'custom-popup'
-					};
-					site.main_photo = Conf.img_srv + 'crop?file=' +  site.main_photo + '&width=50&height=50&type=jpeg'
-					site.marker = newMarker.bindPopup(customPopup, customOptions);
-					newMarker
-						.bindPopup(customPopup, customOptions)
-						.on('mouseover', (ev) => {
-							newMarker.openPopup();
-						})
-						.on('click', (event) => {
-							this.zone.run(() => {
-								this.router.navigate([ '/sites/details/', site.id_site ]);
-							});
-						});
-					this.markers.push(newMarker);
+  getAllSites() {
+    this.spinner.show();
+    this.siteService.getAllSites().subscribe(
+      (sites) => {
+        _.forEach(sites, (site) => {
+          const newMarker = marker(site.geom, {
+            icon: L.icon({
+              iconSize: [25, 41],
+              iconAnchor: [13, 41],
+              iconUrl: './assets/marker-icon.png',
+              shadowUrl: './assets/marker-shadow.png',
+            }),
+          });
+          const customPopup =
+            '<div class="title">' +
+            site.name_site +
+            '</div>' +
+            '<div class=""> <img src=' +
+            Conf.img_srv +
+            'crop?file=' +
+            site.main_photo +
+            '&width=80&height=80&type=jpeg' +
+            '> </div>';
+          const customOptions = {
+            className: 'custom-popup',
+          };
+          site.main_photo =
+            Conf.img_srv +
+            'crop?file=' +
+            site.main_photo +
+            '&width=50&height=50&type=jpeg';
+          site.marker = newMarker.bindPopup(customPopup, customOptions);
+          newMarker
+            .bindPopup(customPopup, customOptions)
+            .on('mouseover', (ev) => {
+              newMarker.openPopup();
+            })
+            .on('click', (event) => {
+              this.zone.run(() => {
+                this.router.navigate(['/sites/details/', site.id_site]);
+              });
+            });
+          this.markers.push(newMarker);
 
-					this.rows.push(
-						_.pick(site, [
-							'main_photo',
-							'name_site',
-							'code_city_site',
-							'publish_site',
-							'geom',
-							'id_site',
-							'marker',
-							'ref_site'
-						])
-					);
-				});
-				this.sitesLoaded = true;
-				this.spinner.hide();
-			},
-			(err) => {
-				this.spinner.hide();
-				this.toastr.error('Une erreur est survenue sur le serveur.', '', {
-					positionClass: 'toast-bottom-right'
-				});
-				console.log('get site error: ', err);
-			}
-		);
-	}
+          this.rows.push(
+            _.pick(site, [
+              'main_photo',
+              'name_site',
+              'code_city_site',
+              'publish_site',
+              'geom',
+              'id_site',
+              'marker',
+              'ref_site',
+            ])
+          );
+        });
+        this.sitesLoaded = true;
+        this.spinner.hide();
+      },
+      (err) => {
+        this.spinner.hide();
+        this.toastr.error('Une erreur est survenue sur le serveur.', '', {
+          positionClass: 'toast-bottom-right',
+        });
+        console.log('get site error: ', err);
+      }
+    );
+  }
 
-	onSelect({ selected }) {
-		this.router.navigate([ '/sites/details/', selected[0].id_site ]);
-	}
+  onSelect({ selected }) {
+    this.router.navigate(['/sites/details/', selected[0].id_site]);
+  }
 
-	onAddSite() {
-		this.router.navigate([ 'sites/form' ]);
-	}
+  onAddSite() {
+    this.router.navigate(['sites/form']);
+  }
 
-	onCenterChange(event) {
-		setTimeout(() => {
-			event.row.marker.openPopup();
-		}, 200);
-		this.center = event.row.geom;
-		this.changeDetector.detectChanges();
-	}
+  onCenterChange(event) {
+    setTimeout(() => {
+      event.row.marker.openPopup();
+    }, 200);
+    this.center = event.row.geom;
+    this.changeDetector.detectChanges();
+  }
 
-	ngOnDestroy() {
-		this.changeDetector.detach();
-		this.spinner.hide();
-	}
+  ngOnDestroy() {
+    this.changeDetector.detach();
+    this.spinner.hide();
+  }
 
-	layerUrl(key, layer) {
-		return (
-			'http://wxs.ign.fr/' +
-			key +
-			'/geoportail/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&' +
-			'LAYER=' +
-			layer +
-			'&STYLE=normal&TILEMATRIXSET=PM&' +
-			'TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&FORMAT=image%2Fjpeg'
-		);
-	}
+  layerUrl(key, layer) {
+    return (
+      'http://wxs.ign.fr/' +
+      key +
+      '/geoportail/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&' +
+      'LAYER=' +
+      layer +
+      '&STYLE=normal&TILEMATRIXSET=PM&' +
+      'TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&FORMAT=image%2Fjpeg'
+    );
+  }
 }
