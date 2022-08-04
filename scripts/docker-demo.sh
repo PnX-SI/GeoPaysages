5222003ee74636beaa6f394ce2a5c54057168559
Vincent Bourgeois <vincent_bourgeois@natural-solutions.eu>
2022-08-04 13:18:37 +0200

    chore: docker demo

---
+++ scripts/docker-demo.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Pre-pre-flight? 🤷
+if [[ -n "$MSYSTEM" ]]; then
+    echo "Seems like you are using an MSYS2-based system (such as Git Bash) which is not supported. Please use WSL instead.";
+    exit 1
+fi
+
+docker compose -f docker-compose-demo.yml --project-name="geopaysages" --project-directory=./docker "$@"
