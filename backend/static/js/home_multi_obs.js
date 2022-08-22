ca1cdaf3a0866f085f8deeb92fc9357d7cb8576d
Vincent Bourgeois <vincent_bourgeois@natural-solutions.eu>
2022-08-22 11:05:49 +0200

    style: code format

--- backend/static/js/home_multi_obs.js
+++ backend/static/js/home_multi_obs.js
@@ -1,6 +1,5 @@
 var geopsg = geopsg || {};
 geopsg.initHome = (options) => {
-
   const map = L.map(document.getElementsByClassName('js-map-wrapper')[0], {
     attributionControl: false,
     boxZoom: false,
@@ -9,28 +8,25 @@ geopsg.initHome = (options) => {
     keyboard: false,
     scrollWheelZoom: false,
     tap: false,
-    zoomControl: false
-  })
-  const tileLayer = L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    zoomControl: false,
+  });
+  const tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }
-  )
-  tileLayer.addTo(map)
+  });
+  tileLayer.addTo(map);
 
-  const lats = []
-  const lons = []
-  options.sites.forEach(site => {
-    lats.push(site.geom[0])
-    lons.push(site.geom[1])
-    L.marker(site.geom).addTo(map)
-    lats.sort()
-    lons.sort()
+  const lats = [];
+  const lons = [];
+  options.sites.forEach((site) => {
+    lats.push(site.geom[0]);
+    lons.push(site.geom[1]);
+    L.marker(site.geom).addTo(map);
+    lats.sort();
+    lons.sort();
     map.fitBounds([
       [lats[0], lons[0]],
-      [lats[lats.length - 1], lons[lons.length - 1]]
-    ])
-  })
-
-}
+      [lats[lats.length - 1], lons[lons.length - 1]],
+    ]);
+  });
+};
