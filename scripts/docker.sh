cee27fa3c81e3f1197bed2ad6256afdade19f416
Vincent Bourgeois <vincent_bourgeois@natural-solutions.eu>
2022-09-14 10:52:45 +0200

    chore: custom docker env

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -8,4 +8,9 @@ if [[ -n "$MSYSTEM" ]]; then
     exit 1
 fi
 
+env_file="docker/.env"
+if ! test -f "$env_file"; then
+    echo "$env_file file doesn't not exists. Create it from docker/.env.tpl. (customize if necessary)"
+    exit 1
+fi
 docker-compose --project-name="geopaysages" --project-directory=./docker "$@"
