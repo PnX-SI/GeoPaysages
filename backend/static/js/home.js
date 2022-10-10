772db1c9e388b1f389a785e0a253bac000abe7f6
Vincent Bourgeois <vincent_bourgeois@natural-solutions.eu>
2022-10-10 13:30:53 +0200

    fix: optional home carousel

--- backend/static/js/home.js
+++ backend/static/js/home.js
@@ -1,9 +1,7 @@
 var geopsg = geopsg || {};
 
 geopsg.initHome = (options) => {
-  if (!options.carousel.photos.length) {
-    document.getElementById('js-app-home-carousel').classList.add('d-none');
-  } else {
+  if (options.carousel.photos.length) {
     new Vue({
       el: '#js-app-home-carousel',
       data: () => {
