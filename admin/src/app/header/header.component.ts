4a7116c076b4ada2c00eaec761864e77211ad286
Vincent Bourgeois <vincent_bourgeois@natural-solutions.eu>
2023-02-28 11:41:43 +0100

    fix: admin logo

--- admin/src/app/header/header.component.ts
+++ admin/src/app/header/header.component.ts
@@ -6,21 +6,20 @@ import { Conf } from './../config';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   isCollapsed = true;
   logoUrl: string;
-  constructor(private loginService: LoginService,
-    protected router: Router, ) { }
+  constructor(private loginService: LoginService, protected router: Router) {}
 
   ngOnInit() {
-    this.logoUrl = `${Conf.customFiles}images/logo_txt_color.png`
+    this.logoUrl = `${Conf.customFiles}images/logo_admin.png`;
   }
 
   logout() {
     this.loginService.logout().subscribe(
-      (res) =>  this.router.navigate(['login']),
+      (res) => this.router.navigate(['login']),
       (err) => {
         console.log('logout', err);
       }
